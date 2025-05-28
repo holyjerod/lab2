25f352ed45f8ba19f50ba6a2a035d2166fd062ec
joshualoh <ltcj06.24@ichat.sp.edu.sg>
2025-05-28 11:34:44 +0800

    secoond version added the kg and m symbol

--- bmi.py
+++ bmi.py
@@ -1,7 +1,7 @@
 def calculate_bmi(height, weight):
     bmi = weight / (height ** 2)
-    print("Height = " + str(height))
-    print("Weight = " + str(weight))
+    print("Height = " + str(height) + "m")
+    print("Weight = " + str(weight) + "kg")
     print("BMI = " + str(round(bmi, 2)))
     return bmi
 
@@ -11,43 +11,7 @@ weight = float(input("Enter your weight in kg: "))
 
 bmi = calculate_bmi(height, weight)
 
-if bmi < 18.5:def calculate_bmi(height, weight):
-    bmi = weight / (height ** 2)
-    print("Height = " + str(height))
-    print("Weight = " + str(weight))
-    print("BMI = " + str(round(bmi, 2)))
-    return bmi
-def calculate_bmi(height, weight):
-    bmi = weight / (height ** 2)
-    print("Height = " + str(height))
-    print("Weight = " + str(weight))
-    print("BMI = " + str(round(bmi, 2)))
-    return bmi
-
-
-height = float(input("Enter your height in meters: "))
-weight = float(input("Enter your weight in kg: "))
-
 if bmi < 18.5:
-    print("under weight")
-elif bmi > 25.0:
-    print("over weight")
-else:
-   print("normal weight")
-
-calculate_bmi(height, weight)
-
-height = float(input("Enter your height in meters: "))
-weight = float(input("Enter your weight in kg: "))
-
-if bmi < 18.5:
-    print("under weight")
-elif bmi > 25.0:
-    print("over weight")
-else:
-   print("normal weight")
-
-calculate_bmi(height, weight)
     print("Underweight")
 elif bmi > 25.0:
     print("Overweight")
